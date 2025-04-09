861d085d34e3f409013759a027e3814f3652a4ce
yashvi-3106 <yashvidholakiya.cg@gmail.com>
2025-04-09 21:58:14 +0530

    fixed server.js

--- Backend/server.js
+++ Backend/server.js
@@ -52,7 +52,7 @@ app.use(cors(corsOptions));
 
 // Handle preflight requests
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin",, 'http://localhost:4200', req.headers.origin || "*");
+  res.header("Access-Control-Allow-Origin", 'http://localhost:4200', req.headers.origin || "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
